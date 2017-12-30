c329cad780cb949d3ee0baa3a5b46f0526f96810
Bill <913010012@qq.com>
2017-12-30 22:50:24 +0800

    2017/12/30 22:51

--- DateTimeAPI/src/cn/csu/local/LocalDateTimeTest.java
+++ DateTimeAPI/src/cn/csu/local/LocalDateTimeTest.java
@@ -4,7 +4,12 @@ import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+
+import org.junit.Test;
 
 public class LocalDateTimeTest {
 
@@ -21,6 +26,41 @@ public class LocalDateTimeTest {
 		// Returns time based on system clock zone Clock defaultClock =
 		Clock.systemDefaultZone();
 		System.out.println("Clock : " + clock.toString());
+	}
+
+	@Test
+	public void interlvalCalc() {
+
+		LocalDate today = LocalDate.now();
+		System.out.println("today : " + today);
+
+		LocalDate birthday = LocalDate.of(1996, 07, 06);
+		System.out.println("birthday : " + birthday);
+		System.out.println("Period: " + Period.between(birthday, today));
+
+		System.out.println("I hava been the world for " + ChronoUnit.DAYS.between(birthday, today) + " days.");
+
+		System.out.println("I hava been the world for " + ChronoUnit.DAYS.between(birthday, today) + " days.");
+
+		System.out.println("-----------------------------------------------------------\n");
+	}
+
+	@Test
+	public void formatTest() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY年MM月dd日 ");
+		LocalDate today = LocalDate.now();
+		String dateStr = today.format(dtf);
+		System.out.println("dateStr:" + dateStr);
+
+		System.out.println("----------------------------------------\n");
+		DateTimeFormatter dateTimeformatter = DateTimeFormatter.ofPattern("YYYY年MM月dd日  HH:mm:ss");
+		LocalDateTime dateTime = LocalDateTime.now();
+		System.out.println("dateTimeStr:" + dateTime.format(dateTimeformatter));
+		
+		System.out.println("----------------------------------------\n");
+		DateTimeFormatter timeformatter = DateTimeFormatter.ofPattern(" HH:mm:ss");
+		LocalTime time = LocalTime.now();
+		System.out.println("dateTimeStr:" + time.format(timeformatter));
 
 	}
 }
